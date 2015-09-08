a5d8deafc0f36b40d772f196f4c7bab9ce0bd92e
iwangx <451749469@qq.com>
2015-09-08 22:22:37 +0800

    2015-09-08

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express=require("express");
-var bodyParser = require('body-parser');
 var path=require("path");
 var routes = require('./routes/index');
 var app = express();
@@ -7,8 +6,6 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'html'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 routes(app);
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
